5db2c877f7a3c8eefefe7bcbaf6e979978c5c27f
d3rupt <thdnkns@gmail.com>
2020-12-04 09:50:58 -0600

    Finally got it with much handholding

--- advent3-1.py
+++ advent3-1.py
@@ -1,29 +1,8 @@
-inputNums = open('input3.txt', 'r').readlines()
-slopesList = []
-firstPass = []
-countRange = []
-trees = 0
-slope1 = 3
-slope2 = 1
-curIndex = 0
-for i in inputNums:
-    nextIndex = inputNums.index(i) + 1
-    i = i.replace('\n', '')
-    print('curIndex: ' + str(curIndex))
-    slope = i[curIndex:curIndex + 3]
-    print('SLOPE LENGTH: ' + str(len(slope)))
-    if len(slope) < 3:
-        print('End of line!')
-        print('slope length: ' + str(len(slope)))
-        slopeDiff = (3 - len(slope))
-        nextLine = inputNums[nextIndex][:slopeDiff]
-        print(nextLine)
-        slope = slope + str(nextLine)
-        print('new slope length: ' + str(len(slope)))
-        curIndex = 0 + (slopeDiff)
-        print('curIndex: ' + str(curIndex))
-    for s in slope:
-        if s == '#':
-            trees += 1
-    curIndex += 3
-print('{} trees' .format(trees))
+
+inputNums = open('input3.txt', 'r').read().splitlines()
+
+
+def num_trees(xdist):
+    return len(list(filter(lambda x: x == "#", [inputNums[y][(xdist * y) % len(inputNums[0])] for y in range(len(inputNums))])))
+
+print('P1: {} trees'.format(num_trees(3)))
